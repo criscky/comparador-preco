cd6e1f1f2f991a0929f641bc8ec1c32036f3742b
criscky <cristianyoshida@ymail.com>
2025-06-27 21:22:31 -0300

    initial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,17 @@
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'comparador-preco';
+export class AppComponent implements OnInit {
+  dados: any[] = [];
+  dadosFiltrados: any[] = [];
+  filtro: any = {
+    nome: '',
+    mes: '',
+    ano: '',
+    codigo: '',
+    valor: ''
+  };
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('assets/valores.json').subscribe(data => {
+      this.dados = data;
+      this.dadosFiltrados = data;
+    });
+  }
+
+  filtrar(): void {
+    this.dadosFiltrados = this.dados.filter(item =>
+      (!this.filtro.nome || item.nome.toLowerCase().includes(this.filtro.nome.toLowerCase())) &&
+      (!this.filtro.mes || item.mes.toLowerCase().includes(this.filtro.mes.toLowerCase())) &&
+      (!this.filtro.ano || item.ano.toLowerCase().includes(this.filtro.ano.toLowerCase())) &&
+      (!this.filtro.codigo || item.codigo.toLowerCase().includes(this.filtro.codigo.toLowerCase())) &&
+      (!this.filtro.valor || item.valor.toString().includes(this.filtro.valor))
+    );
+  }
 }
